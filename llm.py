c4a6ece6aecd18c9b1a99458947cb0822470e769
APSwinner <ague_soria@yahoo.es>
2026-07-15 19:34:59 +0200

    soul

--- llm.py
+++ llm.py
@@ -2,13 +2,16 @@ from ollama import chat
 from pathlib import Path
 
 soul = Path("soul.md")
+
+def read():
+    return soul.read_text(encoding="utf-8")
 def ask(prompt: str) -> str:
     response = chat(
         model="phi3:latest",
         messages=[
             {
                 "role": "system",
-                "content": soul,
+                "content": read(),
             },
             {
                 "role": "user",
--- presence/hackatime.py
+++ presence/hackatime.py
@@ -4,17 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USER = os.getenv("HACKTIME_USER_ID")
+USER = os.getenv("HACKATIME_API_KEY")
 
 def today():    
     url = "https://hackatime.hackclub.com/api/hackatime/v1/users/current/statusbar/today"
-
+    print("API KEY:", USER)  
     headers = {
         "Authorization": f"Bearer {USER}"
     }
 
-    r = requests.get(url, headers=headers, timeout=10)
+    print(headers)
 
+    r = requests.get(url, headers=headers, timeout=10)
+    
+    print(r.status_code)
+    print(r.text)
     if r.status_code != 200:
         print(r.text)
         return None
